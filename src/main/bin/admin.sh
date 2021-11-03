0bbcf4e6bfec38731075f65efa49bc0f153ee495
fmjsjx <fmjsjx@163.com>
2021-11-03 09:54:10 +0800

    Fix java17 & UseEpsilonGC

--- src/main/bin/admin.sh
+++ src/main/bin/admin.sh
@@ -21,7 +21,7 @@ if [ "$1" = "help" ]; then
 fi
 
 # java command
-cmd="/usr/lib/jvm/java-16/bin/java"
+cmd="/usr/lib/jvm/java-17/bin/java"
 
 if [ "x$cmd" = "x" ]; then
   cmd=`type -p java`
@@ -36,8 +36,8 @@ if [ "x$cmd" = "x" ]; then
   fi
   # check java version
   jvm_version=$("$cmd" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-  if [[ "$jvm_version" < "16" ]]; then
-    echo "Error: java16 is required."
+  if [[ "$jvm_version" < "17" ]]; then
+    echo "Error: java17 is required."
     exit 1
   fi
 fi
@@ -59,7 +59,10 @@ PRGDIR=`dirname "$PRG"`
 cd ${PRGDIR}
 
 # Project Version
-PRG_VERSION="1.1.0"
+PRG_VERSION="1.0.0"
+
+GC_OPTS="-XX:+UseEpsilonGC"
+JVM_OPTS="${GC_OPTS}"
 
 # start program
-exec $cmd -jar conveyor-sets-${PRG_VERSION}.jar admin $*
+exec $cmd ${JVM_OPTS} -jar conveyor-sets-${PRG_VERSION}.jar admin $*
